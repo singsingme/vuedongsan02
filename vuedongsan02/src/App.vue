fce81fe70223ffe7275e56b99da8f01543df30f2
LAPTOP-76AB5822\SPOTV WEB <singsingmi@spotv.net>
2022-11-15 20:52:59 +0900

    HTML에 데이터 꽂아넣는 Vue 데이터바인딩 문법

---
+++ vuedongsan02/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <div>
+    <h4>XX 원룸</h4>
+    <p>만원</p>
+  </div>
+
+
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      products : [역삼동원룸, 천호동원룸, 마포구원룸]
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
